89241beb7e20b11690afa05c2948e2510c29cfe4
AkramBitar <akram@il.ibm.com>
2026-06-25 17:00:20 +0000

    Add inbound message size validation
    Signed-off-by: AkramBitar <akram@il.ibm.com>

--- token/services/utils/json/session/envelope.go
+++ token/services/utils/json/session/envelope.go
@@ -169,6 +169,10 @@ func ReceiveTypedWithTimeout(s *session.S, expectedType string, dst any, d time.
 func ReceiveTypedWithTimeoutAndMetrics(s *session.S, expectedType string, dst any, d time.Duration, m *EnvelopeMetrics) error {
 	raw, err := s.ReceiveRawWithTimeout(d)
 	if err != nil {
+		// Surface transport-layer rejections (e.g. oversized messages dropped
+		// before deserialization) in the envelope error metrics.
+		m.observeError(classifyError(err))
+
 		return err
 	}
 
@@ -250,6 +254,8 @@ func (t *TypedSession) ReceiveTypedWithTimeout(expectedType string, dst any, d t
 
 func classifyError(err error) string {
 	switch {
+	case errors.Is(err, session.ErrMessageTooLarge):
+		return "too_large"
 	case errors.Is(err, ErrMissingVersion):
 		return "missing_version"
 	case errors.Is(err, ErrVersionMismatch):
