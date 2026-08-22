82d524f64138db21f117175650be12635af82159
Arunabha Dhal <114598599+arunabha003@users.noreply.github.com>
2026-08-22 12:06:27 +0530

    feat(nwo): add fabricx+evm gateway backend for token integration tests (#2278)
    Signed-off-by: arunabha003 <arunabhadhal04@gmail.com>
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    Signed-off-by: arunabha003 <114598599+arunabha003@users.noreply.github.com>

--- integration/nwo/token/factory.go
+++ integration/nwo/token/factory.go
@@ -41,6 +41,11 @@ func (p *platformFactory) New(ctx api.Context, t api.Topology, builder api.Build
 	tp.AddNetworkHandler(fabricx.PlatformName, tfabric.NewNetworkHandler(tp, builder, fabricxBackend))
 	tp.AddNetworkHandler(tevm.TopologyName, tevm.NewNetworkHandler(tp, builder))
 
+	// The gateway backend reuses the EVM handler; NodeKind makes startNode boot the gateway, not Besu.
+	gatewayHandler := tevm.NewNetworkHandler(tp, builder)
+	gatewayHandler.NodeKind = tevm.GatewayTopologyName
+	tp.AddNetworkHandler(tevm.GatewayTopologyName, gatewayHandler)
+
 	return tp
 }
 
